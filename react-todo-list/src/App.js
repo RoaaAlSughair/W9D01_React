86eb72627f1c8c21e162a511cf38850bdb091df2
RuaaAlsugheir <alsugheirruaa@gmail.com>
2021-05-24 00:21:33 +0300

    Pulse check: Questions 1-6

--- react-todo-list/src/App.js
+++ react-todo-list/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
+const todos = [{todo: "wake up", id: 1}, {todo: "breakfast", id: 2}, {todo: "study", id: 3}];
 
 // jsx
 const App = () => {
-  return <h1>MERAKI Academy</h1>;
+  return <div>
+    <h1>Todo list</h1>
+  </div>;
 };
 
 export default App
---
+++ react-todo-list/src/components/List.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+// jsx
+const List = () => {
+  return <div>
+    
+  </div>;
+};
+
+export default List;
+
+// import React from 'react';
+// // const React=require('react')
+
+// import ReactDOM from 'react-dom';
+// import App from './App';
+
+// ReactDOM.render(
+// <App />, 
+// document.getElementById('root'))
+// ;
